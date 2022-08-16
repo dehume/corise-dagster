3dd4283f1630bc659a44922ded9a806d3a01e2f6
Nemana Satyanarayan Sharma <Nemana.SatyanarayanSharma@schneider-electric.com>
2022-08-17 01:54:41 +0530

    week_1 dagster pipeline assignment

--- week_1/project/week_1.py
+++ week_1/project/week_1.py
@@ -50,16 +50,22 @@ def get_s3_data(context):
     return output
 
 
-@op
-def process_data():
-    pass
+@op(
+out= {"aggregate": Out(dagster_type=Aggregation)},
+description= "Given the list of stocks return the aggregation with the greatest",
+)
+def process_data(stocks: List[Stock]):
+    result = max(stocks, key= lambda p: p.high)
+    return Aggregation(date= result.date, high= result.high)    
 
-
-@op
-def put_redis_data():
+@op(
+description= "Upload an Aggregation to Redis",
+tags={"kind": "redis"}
+)
+def put_redis_data(aggregate: Aggregation):
     pass
 
 
 @job
 def week_1_pipeline():
-    pass
+    put_redis_data(process_data(get_s3_data()))
