8875b1091095dd51351329e6f42c26ee5e5e2fac
Ben <devb3n@gmail.com>
2022-08-17 18:10:43 +0200

    first commit

--- week_1/content/simple_pipeline.py
+++ week_1/content/simple_pipeline.py
@@ -3,12 +3,12 @@ from dagster import graph, op
 
 @op
 def get_name() -> str:
-    return "dagster"
+    return "awesome dagster"
 
 
-# @op
-# def capitalize_name(name: str) -> str:
-#     return name.capitalize()
+@op
+def capitalize_name(name: str) -> str:
+    return name.capitalize()
 
 
 @op
@@ -18,8 +18,8 @@ def hello(name: str):
 
 @graph
 def hello_dagster():
-    # hello(capitalize_name(get_name()))
-    hello(get_name())
+    hello(capitalize_name(get_name()))
+    # hello(get_name())
 
 
 job = hello_dagster.to_job()
