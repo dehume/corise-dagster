b92f577dc7f38e19c7337ef66f126d8adf2ead65
Korntewin Boonchuay <korntewin.b@kbtg.tech>
2022-08-23 22:41:45 +0700

    finish week_2 graph

--- week_2/dagster_ucr/project/week_2.py
+++ week_2/dagster_ucr/project/week_2.py
@@ -1,30 +1,51 @@
 from typing import List
 
 from dagster import In, Nothing, Out, ResourceDefinition, graph, op
+from pkg_resources import require
+from py import process
 from dagster_ucr.project.types import Aggregation, Stock
 from dagster_ucr.resources import mock_s3_resource, redis_resource, s3_resource
 
 
-@op
-def get_s3_data():
-    pass
+@op(
+    required_resource_keys={"s3"},
+    config_schema={"s3_key": str},
+    tags={"kind": "s3"},
+)
+def get_s3_data(context) -> List[Stock]:
+    s3 = context.resources.s3
+    return [
+        Stock.from_list(data)
+        for data in s3.get_data(context.op_config["s3_key"])
+    ]
 
 
 @op
-def process_data():
+def process_data(stocks: List[Stock]) -> Aggregation:
     # Use your op from week 1
-    pass
+    highest = max(stocks, key=lambda stock: stock.high)
+    return Aggregation(
+        date=highest.date,
+        high=highest.high,
+    )
 
 
-@op
-def put_redis_data():
-    pass
+@op(
+    required_resource_keys={"redis"},
+    tags={"kind": "redis"}
+)
+def put_redis_data(context, agg: Aggregation):
+    redis = context.resources.redis
+    redis.put_data(agg.date, agg.high)
+    context.log.info(f"put key: ({agg.date}, val: {agg.high}) to redis")
 
 
 @graph
 def week_2_pipeline():
     # Use your graph from week 1
-    pass
+    stocks = get_s3_data()
+    agg = process_data(stocks)
+    put_redis_data(agg)
 
 
 local = {
@@ -48,7 +69,7 @@ docker = {
             }
         },
     },
-    "ops": {"get_s3_data": {"config": {"s3_key": "preifx/stock.csv"}}},
+    "ops": {"get_s3_data": {"config": {"s3_key": "prefix/stock.csv"}}},
 }
 
 local_week_2_pipeline = week_2_pipeline.to_job(
