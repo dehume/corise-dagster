0560823c9738efe67169295cf004a1b7f64f02a2
jberkowitz89 <jberkowitz89@gmail.com>
2022-08-26 00:34:10 -0600

    committing week 2 project

--- week_2/dagster_ucr/project/week_2.py
+++ week_2/dagster_ucr/project/week_2.py
@@ -1,30 +1,53 @@
 from typing import List
+from operator import attrgetter
 
 from dagster import In, Nothing, Out, ResourceDefinition, graph, op
 from dagster_ucr.project.types import Aggregation, Stock
 from dagster_ucr.resources import mock_s3_resource, redis_resource, s3_resource
 
 
-@op
-def get_s3_data():
-    pass
+@op(
+    config_schema={"s3_key": str},
+    required_resource_keys={"s3"},
+    out={"stocks": Out(dagster_type=List[Stock])},
+    tags={"kind": "s3"},
+    description="Getting a list of stocks from an S3 file",
+)
+def get_s3_data(context):
+    key_name = context.op_config["s3_key"]
+    output = list()
+    for record in context.resources.s3.get_data(key_name):
+        stock = Stock.from_list(record)
+        output.append(stock)
+    return output
 
 
-@op
-def process_data():
-    # Use your op from week 1
-    pass
+@op(
+    ins={"stock_list": In(dagster_type=List[Stock])},
+    out={"agg": Out(dagster_type=Aggregation)},
+    description="Find the highest high value from a list of stocks"
+    )
+def process_data(stock_list) -> Aggregation:
+    high_stock = max(stock_list, key=attrgetter('high'))
+    agg = Aggregation(date=high_stock.date, high=high_stock.high)
+    return agg
 
 
-@op
-def put_redis_data():
-    pass
+@op(
+    required_resource_keys={"redis"},
+    ins={"aggregation": In(dagster_type=Aggregation)},
+    description="Put Aggregation data into Redis",
+    tags={"kind": "redis"}
+    )
+def put_redis_data(context, aggregation: Aggregation):
+    date = str(aggregation.date)
+    high = str(aggregation.high)
+    context.resources.redis.put_data(date, high)
 
 
 @graph
 def week_2_pipeline():
-    # Use your graph from week 1
-    pass
+    put_redis_data(process_data(get_s3_data()))
 
 
 local = {
--- week_2/dagster_ucr/resources.py
+++ week_2/dagster_ucr/resources.py
@@ -91,13 +91,36 @@ def mock_s3_resource():
     return s3_mock
 
 
-@resource
-def s3_resource():
+@resource(
+    config_schema={
+        "bucket": Field(String),
+        "access_key": Field(String),
+        "secret_key": Field(String),
+        "endpoint_url": Field(String),
+    },
+    description="A resource that connects to AWS S3",
+)
+def s3_resource(context) -> S3:
     """This resource defines a S3 client"""
-    pass
+    return S3(
+        bucket=context.resource_config["bucket"],
+        access_key=context.resource_config["access_key"],
+        secret_key=context.resource_config["secret_key"],
+        endpoint_url=context.resource_config["endpoint_url"],
+        )
 
 
-@resource
-def redis_resource():
+@resource(
+    config_schema={
+        "host": Field(String),
+        "port": Field(Int),
+    },
+    description="A resource that connects to Redis",
+)
+def redis_resource(context) -> Redis:
     """This resource defines a Redis client"""
-    pass
+    redis_client = Redis(
+        host=context.resource_config["host"],
+        port=context.resource_config["port"]
+        )
+    return redis_client
