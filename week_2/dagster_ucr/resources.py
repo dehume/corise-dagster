568ccca93d13acef0bb27227bc72326db7272275
Fraser Marlow <fraser@elementl.com>
2022-08-27 17:27:24 -0400

    week 2 submission

--- week_2/dagster_ucr/resources.py
+++ week_2/dagster_ucr/resources.py
@@ -58,6 +58,7 @@ class Redis:
 
 
 # Resources
+
 @resource(
     config_schema={
         "host": Field(String),
@@ -77,7 +78,7 @@ def postgres_resource(context) -> Postgres:
     )
 
 
-@resource
+@resource()
 def mock_s3_resource():
     stocks = [
         ["2020/09/01", "10.0", "10", "10.0", "10.0", "10.0"],
@@ -91,13 +92,33 @@ def mock_s3_resource():
     return s3_mock
 
 
-@resource
-def s3_resource():
-    """This resource defines a S3 client"""
-    pass
+@resource(
+    config_schema={
+        "bucket": Field(String),
+        "access_key": Field(String),
+        "secret_key": Field(String),
+        "endpoint_url": Field(String),
+    },
+    description="S3 resource",
+)
+def s3_resource(context):
+    return S3(
+        bucket=context.resource_config['bucket'],
+        access_key=context.resource_config['access_key'],
+        secret_key=context.resource_config['secret_key'],
+        endpoint_url=context.resource_config['endpoint_url']
+    )
 
 
-@resource
-def redis_resource():
-    """This resource defines a Redis client"""
-    pass
+@resource(
+    config_schema={
+        "host": Field(String),
+        "port": Field(Int),
+    },
+)
+def redis_resource(context):
+    return Redis(
+        host=context.resource_config['host'],
+        port=context.resource_config['port']
+    )
+
