dfa5c7a26da06196e283d649d2c50ef195a4803f
Serge Gekker <sgekker429@gmail.com>
2022-08-27 13:21:33 -0400

    completes week_2 pipeline

--- week_2/dagster_ucr/project/week_2.py
+++ week_2/dagster_ucr/project/week_2.py
@@ -3,29 +3,55 @@ from typing import List
 from dagster import In, Nothing, Out, ResourceDefinition, graph, op
 from dagster_ucr.project.types import Aggregation, Stock
 from dagster_ucr.resources import mock_s3_resource, redis_resource, s3_resource
+from dagster import get_dagster_logger
+from datetime import datetime as dt
+
+logger = get_dagster_logger()
 
 
-@op
-def get_s3_data():
-    pass
+@op(required_resource_keys={"s3"},
+    config_schema={"s3_key": str},
+    out={"stocks": Out(dagster_type=List[Stock])},
+    tags={"kind": "s3"},
+    description="Get a list of stocks from an S3 file",
+)
+def get_s3_data(context):
+    output = list()
+    data = context.resources.s3.get_data(context.op_config["s3_key"])
+    for row in data:
+        stock = Stock.from_list(row)
+        output.append(stock)
+    return output
 
 
-@op
-def process_data():
-    # Use your op from week 1
-    pass
+@op(
+    ins={"stocks": In(dagster_type=List[Stock])},
+    out={"aggregation": Out(dagster_type=Aggregation)},
+    description="Aggregate stock data",
+)
+def process_data(stocks: List[Stock]) -> Aggregation:
+    top_high = sorted(stocks, key=lambda x: x.high, reverse=True)[0]
+    return Aggregation(date=top_high.date, high=top_high.high)
 
 
-@op
-def put_redis_data():
-    pass
+@op(required_resource_keys={"redis"},
+    ins={"aggregation": In(dagster_type=Aggregation)},
+    out=Out(Nothing),
+    tags={"kind": "redis"},
+    description="Upload aggregation to Redis",
+)
+def put_redis_data(context,aggregation: Aggregation):
+    agg_date = dt.strftime(aggregation.date, '%m/%d/%Y')
+    agg_high = str(aggregation.high)
+    context.resources.redis.put_data(agg_date, agg_high)
+    logger.info(f"Date: {agg_date} with daily high of ${agg_high} stored on Redis")
 
 
 @graph
 def week_2_pipeline():
-    # Use your graph from week 1
-    pass
-
+    stocks = get_s3_data()
+    aggregation = process_data(stocks)
+    put_redis_data(aggregation)
 
 local = {
     "ops": {"get_s3_data": {"config": {"s3_key": "prefix/stock.csv"}}},
