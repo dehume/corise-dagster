121dd7a0f26a515121f5773950dbe1cbcfb6fb27
Serge Gekker <sgekker429@gmail.com>
2022-09-01 15:19:55 -0400

    week 3 initial commit

--- week_3/project/week_3.py
+++ week_3/project/week_3.py
@@ -1,3 +1,4 @@
+
 from typing import List
 
 from dagster import (
@@ -9,6 +10,7 @@ from dagster import (
     RunRequest,
     ScheduleDefinition,
     SkipReason,
+    get_dagster_logger,
     graph,
     op,
     sensor,
@@ -18,29 +20,47 @@ from project.resources import mock_s3_resource, redis_resource, s3_resource
 from project.sensors import get_s3_keys
 from project.types import Aggregation, Stock
 
+logger = get_dagster_logger()
 
 @op
-def get_s3_data():
-    # Use your ops from week 2
-    pass
+def get_s3_data(context):
+    output = list()
+    data = context.resources.s3.get_data(context.op_config["s3_key"])
+    for row in data:
+        stock = Stock.from_list(row)
+        output.append(stock)
+    return output
 
 
-@op
-def process_data():
-    # Use your ops from week 2
-    pass
+@op(
+    ins={"stocks": In(dagster_type=List[Stock])},
+    out={"aggregation": Out(dagster_type=Aggregation)},
+    description="Aggregate stock data",
+)
+def process_data(stocks: List[Stock]) -> Aggregation:
+    top_high = sorted(stocks, key=lambda x: x.high, reverse=True)[0]
+    return Aggregation(date=top_high.date, high=top_high.high)
 
 
-@op
-def put_redis_data():
-    # Use your ops from week 2
-    pass
+@op(required_resource_keys={"redis"},
+    ins={"aggregation": In(dagster_type=Aggregation)},
+    out=Out(Nothing),
+    tags={"kind": "redis"},
+    description="Upload aggregation to Redis",
+)
+def put_redis_data(context,aggregation: Aggregation):
+    agg_date = dt.strftime(aggregation.date, '%m/%d/%Y')
+    agg_high = str(aggregation.high)
+    context.resources.redis.put_data(agg_date, agg_high)
+    logger.info(f"Date: {agg_date} with daily high of ${agg_high} stored on Redis")
+
 
 
 @graph
 def week_3_pipeline():
-    # Use your graph from week 2
-    pass
+    stocks = get_s3_data()
+    aggregation = process_data(stocks)
+    put_redis_data(aggregation)
 
 
 local = {
@@ -69,8 +89,26 @@ docker = {
 }
 
 
-def docker_config():
-    pass
+def docker_config(s3_key: str):
+    return {
+    "resources": {
+        "s3": {
+            "config": {
+                "bucket": "dagster",
+                "access_key": "test",
+                "secret_key": "test",
+                "endpoint_url": "http://host.docker.internal:4566",
+            }
+        },
+        "redis": {
+            "config": {
+                "host": "redis",
+                "port": 6379,
+            }
+        },
+    },
+    "ops": {"get_s3_data": {"config": {"s3_key": s3_key}}},
+}
 
 
 local_week_3_pipeline = week_3_pipeline.to_job(
@@ -84,19 +122,29 @@ local_week_3_pipeline = week_3_pipeline.to_job(
 
 docker_week_3_pipeline = week_3_pipeline.to_job(
     name="docker_week_3_pipeline",
-    config=docker_config,
+    config=docker,
     resource_defs={
         "s3": s3_resource,
         "redis": redis_resource,
     },
 )
 
+#This schedule should run the local_week_3_pipeline every 15 minutes.
+local_week_3_schedule = ScheduleDefinition(job=local_week_3_pipeline, cron_schedule="*/15 * * * *")
 
-local_week_3_schedule = None  # Add your schedule
+#This schedule should run the docker_week_3_pipeline at the beginning of every hour.
+docker_week_3_schedule = ScheduleDefinition(job=docker_week_3_pipeline, cron_schedule="0 * * * *")
 
-docker_week_3_schedule = None  # Add your schedule
-
-
-@sensor
-def docker_week_3_sensor():
-    pass
+@sensor(job=docker_week_3_pipeline)
+def docker_week_3_sensor(context):
+    new_files = get_s3_keys(
+        bucket="dagster",
+        prefix="prefix"
+    )
+    if not new_files:
+        yield SkipReason("No new files found on S3 bucket")
+    for new_file in new_files:
+        yield RunRequest(
+            run_key=new_file,
+            run_config=docker_config(new_file)
+        )
