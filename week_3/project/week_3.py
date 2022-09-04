5cc6968db1d6ff72481fcb274969ef89badd6c9f
David Gibbons <dgibbons@LM-DGIBBONS.local>
2022-09-04 16:51:59 -0400

    all but one pass

--- week_3/project/week_3.py
+++ week_3/project/week_3.py
@@ -1,5 +1,7 @@
-from typing import List
 
+from .sensors import get_s3_keys
+
+from datetime import datetime
 from dagster import (
     In,
     Nothing,
@@ -69,7 +71,7 @@ def put_redis_data(context, aggregation):
 
 
 @graph
-def week_2_pipeline():
+def week_3_pipeline():
     put_redis_data(process_data(get_s3_data()))
    
 
@@ -96,12 +98,37 @@ docker = {
             }
         },
     },
-    "ops": {"get_s3_data": {"config": {"s3_key": "prefix/stock_9.csv"}}},
-}
+    "ops":  { 
+        "get_s3_data": {"config": {"s3_key": 'prefix/{partition_key}'}},
+    }
+  }
 
 
-def docker_config():
-    pass
+@static_partitioned_config(partition_keys=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])
+def docker_config(partition_key: str):
+    return {
+            'resources' : {
+             'redis': {'config': 
+             {'host': 'redis', 'port': 6379
+              }
+            }, 
+          's3': {'config': 
+              {
+                  'access_key': 'test', 
+                  'bucket': 'dagster', 
+                  'endpoint_url': 'http://host.docker.internal:4566', 
+                  'secret_key': 'test'
+              }
+            },
+         },
+        'ops': {
+            "get_s3_data": {
+                "config": {
+                    "s3_key": partition_key
+                    }
+                }
+            },
+    } 
 
 
 local_week_3_pipeline = week_3_pipeline.to_job(
@@ -120,14 +147,28 @@ docker_week_3_pipeline = week_3_pipeline.to_job(
         "s3": s3_resource,
         "redis": redis_resource,
     },
+    op_retry_policy=RetryPolicy(max_retries=10, delay=1),
 )
 
 
-local_week_3_schedule = None  # Add your schedule
+local_week_3_schedule = ScheduleDefinition(job=local_week_3_pipeline, cron_schedule="*/15 * * * *")
 
-docker_week_3_schedule = None  # Add your schedule
+docker_week_3_schedule = ScheduleDefinition(job=docker_week_3_pipeline, cron_schedule="0 * * * *")
 
 
-@sensor
-def docker_week_3_sensor():
-    pass
+@sensor(job=week_3_pipeline, minimum_interval_seconds=30)
+def docker_week_3_sensor(context):
+    new_files = get_s3_keys(
+        bucket="dagster",
+        prefix="prefix"
+    )
+    if not new_files:
+        yield SkipReason("No new s3 files found in bucket.")
+        return
+    for new_file in new_files:
+        yield RunRequest(
+            run_key=new_file,
+            run_config= docker_config(new_file)
+        )
+
+
