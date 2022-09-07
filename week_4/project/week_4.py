ed1277631400b86f973249001785956845772465
Oren Lederman <oren@apella.io>
2022-09-07 13:37:21 -0700

    Week 4 - software assets

--- week_4/project/week_4.py
+++ week_4/project/week_4.py
@@ -1,26 +1,62 @@
 from typing import List
+from operator import attrgetter
 
-from dagster import Nothing, asset, with_resources
-from project.resources import redis_resource, s3_resource
+
+from dagster import Nothing, asset, with_resources, OpExecutionContext
+from project.resources import redis_resource, s3_resource, S3, Redis
 from project.types import Aggregation, Stock
 
 
-@asset
-def get_s3_data():
-    # Use your op logic from week 3
-    pass
+@asset(
+    group_name="corise",
+    required_resource_keys={"s3"},
+    config_schema={"s3_key": str},
+    op_tags={"kind": "s3"},
+    description="List of Stocks",
+)
+def get_s3_data(context: OpExecutionContext) -> List[Stock]:
+    s3: S3 = context.resources.s3  # assinged to a variable for better typing support
+    data = s3.get_data(key_name=context.op_config["s3_key"])
+
+    output = [Stock.from_list(row) for row in data]
+    return output
 
 
-@asset
-def process_data():
+@asset(group_name="corise", description="keeps the Stock with the greatest `high` value")
+def process_data(get_s3_data: List[Stock]) -> Aggregation:
+    highest_stock: Stock = max(get_s3_data, key=attrgetter("high"))
+    return Aggregation(date=highest_stock.date, high=highest_stock.high)
+
+
+@asset(
+    group_name="corise",
+    required_resource_keys={"redis"},
+    op_tags={"kind": "redis"},
+    description="persists the results to redis",
+)
+def put_redis_data(context: OpExecutionContext, process_data: Aggregation) -> Nothing:
     # Use your op logic from week 3 (you will need to make a slight change)
-    pass
+    redis: Redis = context.resources.redis  # assinged to a variable for better typing support
+    redis.put_data(name=process_data.date.isoformat(), value=str(process_data.high))
 
 
-@asset
-def put_redis_data():
-    # Use your op logic from week 3 (you will need to make a slight change)
-    pass
-
-
-get_s3_data_docker, process_data_docker, put_redis_data_docker = with_resources()
+get_s3_data_docker, process_data_docker, put_redis_data_docker = with_resources(
+    definitions=[get_s3_data, process_data, put_redis_data],
+    resource_defs={"s3": s3_resource, "redis": redis_resource},
+    resource_config_by_key={
+        "s3": {
+            "config": {
+                "bucket": "dagster",
+                "access_key": "test",
+                "secret_key": "test",
+                "endpoint_url": "http://localstack:4566",
+            }
+        },
+        "redis": {
+            "config": {
+                "host": "redis",
+                "port": 6379,
+            }
+        },
+    },
+)
