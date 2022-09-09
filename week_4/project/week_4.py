c250468b3f5691a9a9023303fa4635cf90e708ea
Josh Braun <josh.braun.4@gmail.com>
2022-09-09 13:21:32 -0400

    pass tests

--- week_4/project/week_4.py
+++ week_4/project/week_4.py
@@ -6,10 +6,10 @@ from project.types import Aggregation, Stock
 
 
 @asset(
+    group_name="corise",
     config_schema={"s3_key": str},
     required_resource_keys={"s3"},
-    description="Upload highest stock to Redis",
-    op_tags={"kind": "s3"},
+    description="Get a list of stocks from an S3 file",
 )
 def get_s3_data(context):
     stocks = []
@@ -19,18 +19,21 @@ def get_s3_data(context):
 
 
 @asset(
-    op_tags={"kind": "python"},
-    description="Return the stock with the highest value"
+    group_name="corise",
+    description="Get an aggregation of the highest stock",
 )
 def process_data(stocks: List[Stock]) -> Aggregation:
-    highest_stock = max(stocks, key=lambda stock: stock.high)
-    return Aggregation(date=highest_stock.date, high=highest_stock.high)
+    """
+    Returns the aggregated Stock with the greatest high value
+    from a list of Stocks
+    """
+    max_stock = max(stocks, key=lambda stock: stock.high)
+    return Aggregation(date=max_stock.date, high=max_stock.high)
 
 
 @asset(
+    group_name="corise",
     required_resource_keys={"redis"},
-    op_tags={"kind": "Redis"},
-    description="Return the stock with the highest value"
 )
 def put_redis_data(context, max_stock: Aggregation):
     context.resources.redis.put_data(str(max_stock.date), str(max_stock.high))
@@ -38,24 +41,20 @@ def put_redis_data(context, max_stock: Aggregation):
 
 get_s3_data_docker, process_data_docker, put_redis_data_docker = with_resources(
     definitions=[get_s3_data, process_data, put_redis_data],
-    resource_defs={
-        "s3": s3_resource,
-        "redis": redis_resource
-    },
+    resource_defs={"s3": s3_resource, "redis": redis_resource},
     resource_config_by_key={
         "s3": {
             "config": {
                 "bucket": "dagster",
                 "access_key": "test",
                 "secret_key": "test",
-                "endpoint_url": "http://localstack:4566",
+                "endpoint_url": "http://host.docker.internal:4566",
             }
         },
-        "redis": {
-            "config": {
-                "host": "redis",
-                "port": 6379,
-            }
-        },
-    }
+        "redis": { "config": {
+            "host": "redis",
+            "port": 6379
+            },
+        }
+    },
 )
