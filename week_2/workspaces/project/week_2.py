f5524352037fda46072b56c19a77361c3d527272
Frank Lü <lu.phrank@gmail.com>
2022-11-28 07:00:58 +0000

    finished week2 resources.

--- week_2/workspaces/project/week_2.py
+++ week_2/workspaces/project/week_2.py
@@ -5,29 +5,46 @@ from workspaces.resources import mock_s3_resource, redis_resource, s3_resource
 from workspaces.types import Aggregation, Stock
 
 
-@op
-def get_s3_data():
-    pass
+@op(
+    config_schema={
+        "s3_key": String
+    },
+    required_resource_keys={"s3"}
+)
+def get_s3_data(context) -> List[Stock]:
+    s3_key = context.op_config["s3_key"]
+    return [Stock.from_list(item) for item in context.resources.s3.get_data(s3_key)]
 
 
-@op
-def process_data():
-    pass
+@op (
+    ins={"stocks": In(dagster_type=List[Stock])},
+    out={"agg": Out(dagster_type=Aggregation)}
+)
+def process_data(context, stocks: List[Stock]) -> Aggregation:
+    highest_stock = max(stocks, key= lambda stock: stock.high)
+    return Aggregation(date=highest_stock.date,high=highest_stock.high)
 
 
-@op
-def put_redis_data():
-    pass
+@op(
+    required_resource_keys={"redis"}
+)
+def put_redis_data(context, agg: Aggregation) -> Nothing:
+    context.resources.redis.put_data(name=str(agg.date), value=str(agg.high))
 
 
-@op
-def put_s3_data():
-    pass
+@op(
+    required_resource_keys={"s3"}
+)
+def put_s3_data(context, agg: Aggregation) -> Nothing:
+    context.resources.s3.put_data(key=agg.date, data=agg)
 
 
 @graph
 def week_2_pipeline():
-    pass
+    stocks = get_s3_data()
+    processed = process_data(stocks)
+    put_redis_data(processed)
+    put_s3_data(processed)
 
 
 local = {
@@ -56,8 +73,18 @@ docker = {
 
 week_2_pipeline_local = week_2_pipeline.to_job(
     name="week_2_pipeline_local",
+    config= local,
+    resource_defs = {
+        "s3": mock_s3_resource,
+        "redis": ResourceDefinition.mock_resource()
+    }
 )
 
 week_2_pipeline_docker = week_2_pipeline.to_job(
     name="week_2_pipeline_docker",
+    config= docker,
+    resource_defs = {
+        "s3": s3_resource,
+        "redis": redis_resource
+    }
 )
