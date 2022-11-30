02f8dcb82277fd3d1a1fd61f1429aa82a787f16a
jpbianchi <jpbianchi@gmail.com>
2022-11-30 20:46:35 +0100

    Week2 project done

--- week_2/workspaces/project/week_2.py
+++ week_2/workspaces/project/week_2.py
@@ -5,29 +5,34 @@ from workspaces.resources import mock_s3_resource, redis_resource, s3_resource
 from workspaces.types import Aggregation, Stock
 
 
-@op
-def get_s3_data():
-    pass
+@op(config_schema={"s3_key": String},
+    required_resource_keys={"s3"}
+)
+def get_s3_data(context) -> List[Stock]:
+    s3bucket = context.op_config["s3_key"]
+    return [Stock.from_list(st) for st in context.resources.s3.get_data(s3bucket)]
 
 
 @op
-def process_data():
-    pass
+def process_data(context, stocks: List[Stock]) -> Aggregation:
+    highestock = max(stocks, key=lambda st: st.high)
+    return Aggregation(date=highestock.date, high=highestock.high)
 
 
-@op
-def put_redis_data():
-    pass
+@op(required_resource_keys={"redis"})
+def put_redis_data(context, highest: Aggregation) -> Nothing:
+    context.resources.redis.put_data(str(highest.date), str(highest.high))
 
 
-@op
-def put_s3_data():
-    pass
+@op(required_resource_keys={"s3"})
+def put_s3_data(context, highest: Aggregation) -> Nothing:
+    s3bucket = get_s3_data.config_field.config_type.fields["s3_key"],  # haha, this was a tricky one!
+    context.resources.s3.put_data(s3bucket, highest)
 
 
 @graph
 def week_2_pipeline():
-    pass
+    put_redis_data(process_data(get_s3_data()))
 
 
 local = {
@@ -56,8 +61,12 @@ docker = {
 
 week_2_pipeline_local = week_2_pipeline.to_job(
     name="week_2_pipeline_local",
+    config=local,
+    resource_defs={"s3": mock_s3_resource, "redis": ResourceDefinition.mock_resource()}
 )
 
 week_2_pipeline_docker = week_2_pipeline.to_job(
     name="week_2_pipeline_docker",
+    config=docker,
+    resource_defs={"s3": s3_resource, "redis": redis_resource}
 )
