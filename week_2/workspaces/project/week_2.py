09c49736a26b6f9a5fb2f4e0f8472a206ec95132
Derek Hecksher <derek.hecksher@figment.io>
2022-12-05 06:16:58 +0000

    Week 2 updates

--- week_2/workspaces/project/week_2.py
+++ week_2/workspaces/project/week_2.py
@@ -5,33 +5,49 @@ from workspaces.resources import mock_s3_resource, redis_resource, s3_resource
 from workspaces.types import Aggregation, Stock
 
 
-@op
-def get_s3_data():
-    pass
+@op(
+    config_schema={"s3_key": String},
+    required_resource_keys={"s3"},
+    description="Get a list of stocks from an S3 file"
+)
+def get_s3_data(context) -> List[Stock]:
+    return [Stock.from_list(record) for record in context.resources.s3.get_data(context.op_config["s3_key"])]
 
 
-@op
-def process_data():
-    pass
+@op(
+    description="Given a list of stocks, return the Aggregation with the greatest high value"
+)
+def process_data(context, stocks: List[Stock]) -> Aggregation:
+    sorted_stocks = sorted(stocks, key=lambda stock: stock.high, reverse=True)
+    return Aggregation(date = sorted_stocks[0].date, high = sorted_stocks[0].high)
 
 
-@op
-def put_redis_data():
-    pass
+@op(
+    required_resource_keys={"redis"},
+    description="Upload the Aggregation to Redis"
+)
+def put_redis_data(context, stock: Aggregation):
+    context.resources.redis.put_data(stock.date, stock.high)
 
 
-@op
-def put_s3_data():
-    pass
+@op(
+    required_resource_keys={"s3"},
+    description="Upload the Aggregation to S3"
+)
+def put_s3_data(context, stock: Aggregation):
+    context.resources.s3.put_data("stocks/{date}/{high}".format(date = stock.date, high = stock.high), stock)
 
 
 @graph
 def week_2_pipeline():
-    pass
+    s3_data = get_s3_data()
+    processed_data = process_data(s3_data)
+    put_redis_data(processed_data)
+    put_s3_data(processed_data)
 
 
 local = {
-    "ops": {"get_s3_data": {"config": {"s3_key": "prefix/stock.csv"}}},
+    "ops": {"get_s3_data": {"config": {"s3_key": "week_2/data/stock.csv"}}},
 }
 
 docker = {
@@ -51,13 +67,23 @@ docker = {
             }
         },
     },
-    "ops": {"get_s3_data": {"config": {"s3_key": "prefix/stock.csv"}}},
+    "ops": {"get_s3_data": {"config": {"s3_key": "week_2/data/stock.csv"}}},
 }
 
 week_2_pipeline_local = week_2_pipeline.to_job(
     name="week_2_pipeline_local",
+    config=local,
+    resource_defs={
+        "s3": mock_s3_resource,
+        "redis": ResourceDefinition.mock_resource()
+    }
 )
 
 week_2_pipeline_docker = week_2_pipeline.to_job(
     name="week_2_pipeline_docker",
+    config=docker,
+    resource_defs={
+        "s3": s3_resource,
+        "redis": redis_resource
+    }
 )
