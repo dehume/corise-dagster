cdc40b654a906cc5e706103e46bddb6a01454e04
Amardeep Singh <amardeeps@gmail.com>
2022-12-11 22:45:23 +0530

    Week 3 changes

--- week_3/workspaces/project/week_3.py
+++ week_3/workspaces/project/week_3.py
@@ -8,6 +8,7 @@ from dagster import (
     RetryPolicy,
     RunRequest,
     ScheduleDefinition,
+    SensorEvaluationContext,
     SkipReason,
     graph,
     op,
@@ -20,34 +21,39 @@ from workspaces.resources import mock_s3_resource, redis_resource, s3_resource
 from workspaces.types import Aggregation, Stock
 
 
-@op
-def get_s3_data():
-    # You can reuse the logic from the previous week
-    pass
+@op(config_schema={"s3_key": str}, required_resource_keys={"s3"})
+def get_s3_data(context) -> List[Stock]:
+    s3_key = context.op_config["s3_key"]
+    context.log.info(f"Getting {s3_key} from S3")
+    records = context.resources.s3.get_data(s3_key)
+    stocks = [Stock.from_list(r) for r in records]
+    return stocks
 
 
 @op
-def process_data():
-    # You can reuse the logic from the previous week
-    pass
+def process_data(context, stocks: List[Stock]) -> Aggregation:
+    max_stock = max(stocks, key=lambda stock: stock.high)
+    return Aggregation(date=max_stock.date, high=max_stock.high)
 
 
-@op
-def put_redis_data():
-    # You can reuse the logic from the previous week
-    pass
+@op(required_resource_keys={"redis"})
+def put_redis_data(context, agg: Aggregation):
+    context.log.info(f"Putting {agg} into Redis")
+    context.resources.redis.put_data(agg.date.isoformat(), f"{agg.high}")
 
 
-@op
-def put_s3_data():
-    # You can reuse the logic from the previous week
-    pass
+@op(required_resource_keys={"s3"})
+def put_s3_data(context, agg: Aggregation):
+    s3_key = agg.date.isoformat()
+    context.log.info(f"Putting {agg} into S3 at {s3_key}")
+    context.resources.s3.put_data(s3_key, agg)
 
 
 @graph
 def week_3_pipeline():
-    # You can reuse the logic from the previous week
-    pass
+    agg = process_data(get_s3_data())
+    put_redis_data(agg)
+    put_s3_data(agg)
 
 
 local = {
@@ -75,28 +81,66 @@ docker = {
     "ops": {"get_s3_data": {"config": {"s3_key": "prefix/stock_9.csv"}}},
 }
 
+PARTITIONS = [str(i) for i in range(1, 11)]
 
-def docker_config():
-    pass
+
+@static_partitioned_config(partition_keys=PARTITIONS)
+def docker_config(partition_key: str):
+    cfg = docker.copy()
+    cfg["ops"]["get_s3_data"]["config"]["s3_key"] = f"prefix/stock_{partition_key}.csv"
+    return cfg
 
 
 week_3_pipeline_local = week_3_pipeline.to_job(
     name="week_3_pipeline_local",
+    config=local,
+    resource_defs={
+        "s3": mock_s3_resource,
+        "redis": ResourceDefinition.mock_resource(),
+    },
 )
 
 week_3_pipeline_docker = week_3_pipeline.to_job(
     name="week_3_pipeline_docker",
+    config=docker_config,
+    resource_defs={
+        "s3": s3_resource,
+        "redis": redis_resource,
+    },
+    op_retry_policy=RetryPolicy(max_retries=10, delay=1),
 )
 
 
-week_3_schedule_local = None
+week_3_schedule_local = ScheduleDefinition(job=week_3_pipeline_local, cron_schedule="*/15 * * * *")
 
 
-@schedule
+@schedule(job=week_3_pipeline_docker, cron_schedule="0 * * * *")
 def week_3_schedule_docker():
-    pass
+    for partition in PARTITIONS:
+        yield week_3_pipeline_docker.run_request_for_partition(partition_key=partition, run_key=partition)
 
 
-@sensor
-def week_3_sensor_docker():
-    pass
+# TODO: Not sure best way to store these.
+BUCKET = "dagster"
+PREFIX = "prefix"
+ENDPOINT_URL = "http://localstack:4566"
+
+
+@sensor(job=week_3_pipeline_docker, minimum_interval_seconds=30)
+def week_3_sensor_docker(context: SensorEvaluationContext):
+    since_key = context.cursor
+    
+    new_keys = get_s3_keys(bucket=BUCKET, prefix=PREFIX, endpoint_url=ENDPOINT_URL, since_key=since_key)
+    # TODO: How to log in sensor?
+    print(f"Found new keys: {new_keys}")
+
+    if not new_keys:
+        yield SkipReason("No new s3 files found in bucket.")
+        return
+
+    for key in new_keys:
+        cfg = docker.copy()
+        cfg["ops"]["get_s3_data"]["config"]["s3_key"] = key
+        yield RunRequest(run_key=key, run_config=cfg)
+
+    context.update_cursor(new_keys[-1])
