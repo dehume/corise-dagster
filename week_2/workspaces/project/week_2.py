1a0037da9eebd4b7f6757fa533ae79e73e643b82
Amardeep Singh <amardeeps@gmail.com>
2022-12-11 16:44:29 +0530

    Week 2 changes

--- week_2/workspaces/project/week_2.py
+++ week_2/workspaces/project/week_2.py
@@ -5,29 +5,39 @@ from workspaces.resources import mock_s3_resource, redis_resource, s3_resource
 from workspaces.types import Aggregation, Stock
 
 
-@op
-def get_s3_data():
-    pass
+@op(config_schema={"s3_key": str}, required_resource_keys={"s3"})
+def get_s3_data(context) -> list[Stock]:
+    s3_key = context.op_config["s3_key"]
+    context.log.info(f"Getting {s3_key} from S3")
+    records = context.resources.s3.get_data(s3_key)
+    stocks = [Stock.from_list(r) for r in records]
+    return stocks
 
 
 @op
-def process_data():
-    pass
+def process_data(context, stocks: list[Stock]) -> Aggregation:
+    max_stock = max(stocks, key=lambda stock: stock.high)
+    return Aggregation(date=max_stock.date, high=max_stock.high)
 
 
-@op
-def put_redis_data():
-    pass
+@op(required_resource_keys={"redis"})
+def put_redis_data(context, agg: Aggregation):
+    context.log.info(f"Putting {agg} into Redis")
+    context.resources.redis.put_data(agg.date.isoformat(), f"{agg.high}")
 
 
-@op
-def put_s3_data():
-    pass
+@op(required_resource_keys={"s3"})
+def put_s3_data(context, agg: Aggregation):
+    s3_key = agg.date.isoformat()
+    context.log.info(f"Putting {agg} into S3 at {s3_key}")
+    context.resources.s3.put_data(s3_key, agg)
 
 
 @graph
 def week_2_pipeline():
-    pass
+    agg = process_data(get_s3_data())
+    put_redis_data(agg)
+    put_s3_data(agg)
 
 
 local = {
@@ -56,8 +66,18 @@ docker = {
 
 week_2_pipeline_local = week_2_pipeline.to_job(
     name="week_2_pipeline_local",
+    config=local,
+    resource_defs={
+        "s3": mock_s3_resource,
+        "redis": ResourceDefinition.mock_resource(),
+    },
 )
 
 week_2_pipeline_docker = week_2_pipeline.to_job(
     name="week_2_pipeline_docker",
+    config=docker,
+    resource_defs={
+        "s3": s3_resource,
+        "redis": redis_resource,
+    },
 )
--- week_2/workspaces/resources.py
+++ week_2/workspaces/resources.py
@@ -98,13 +98,36 @@ def mock_s3_resource(context):
     return s3_mock
 
 
-@resource
-def s3_resource():
+@resource(
+    config_schema={
+        "bucket": Field(String),
+        "access_key": Field(String),
+        "secret_key": Field(String),
+        "endpoint_url": Field(String, is_required=False),
+    },
+    description="A resource to access S3 buckets",
+)
+def s3_resource(context):
     """This resource defines a S3 client"""
-    pass
+    return S3(
+        bucket=context.resource_config["bucket"],
+        access_key=context.resource_config["access_key"],
+        secret_key=context.resource_config["secret_key"],
+        endpoint_url=context.resource_config.get("endpoint_url"),
+    )
 
 
-@resource
-def redis_resource():
+@resource(
+    config_schema={
+        "host": Field(String),
+        "port": Field(Int),
+    },
+    description="A resource to access Redis",
+)
+def redis_resource(context):
     """This resource defines a Redis client"""
-    pass
+    return Redis(
+        host=context.resource_config["host"],
+        port=context.resource_config["port"],
+    )
+
